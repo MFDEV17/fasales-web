a1ab24f92d1b601973d2b5835cf2821f6dfbf330
Maxim <maximsushkin02@gmail.com>
2024-09-10 03:48:51 +0300

    init tailwind

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -14,6 +14,7 @@ export default defineNuxtConfig({
         },
       },
     ],
+    '@nuxtjs/tailwindcss',
   ],
   sanity: {
     projectId: process.env.NUXT_SANITY_PROJECT_ID,
@@ -26,6 +27,52 @@ export default defineNuxtConfig({
       stega: true,
     },
   },
+  tailwindcss: {
+    config: {
+      theme: {
+        keyframes: {
+          overlayShow: {
+            from: { opacity: '0' },
+            to: { opacity: '1' },
+          },
+          overlayHide: {
+            from: { opacity: '1' },
+            to: { opacity: '0' },
+          },
+          contentShow: {
+            from: {
+              opacity: '0',
+              transform: 'translate(-50%, -48%) scale(0.96)',
+            },
+            to: { opacity: '1', transform: 'translate(-50%, -50%) scale(1)' },
+          },
+        },
+        animation: {
+          overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+          overlayHide: 'overlayHide 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+          contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        },
+        screens: {
+          md: '393px',
+        },
+        colors: {
+          failure: '#F25A5A',
+          black: '#000000',
+          telegram: {
+            link: '#5AC8FB',
+            hint: '#8B8B8B',
+            text: '#020202',
+            btn: '#5AC8FB',
+            'btn-text': '#FFFFFF',
+            bg: {
+              primary: '#FFFFFF',
+              secondary: '#F3F2F8',
+            },
+          },
+        },
+      },
+    },
+  },
   postcss: {
     plugins: {
       autoprefixer: {},
@@ -33,3 +80,4 @@ export default defineNuxtConfig({
     },
   },
 })
+
