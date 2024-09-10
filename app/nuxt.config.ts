b940e9f8b271399f66ab596228e63e8691094782
Maxim <maximsushkin02@gmail.com>
2024-09-10 10:22:13 +0300

    init radix

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     '@nuxt/image',
     '@pinia/nuxt',
+    'radix-vue/nuxt',
   ],
   sanity: {
     projectId: process.env.NUXT_SANITY_PROJECT_ID,
@@ -64,3 +65,4 @@ export default defineNuxtConfig({
     },
   },
 })
+
