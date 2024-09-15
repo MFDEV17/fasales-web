13ef022c245b48da31668a2e1dfdbb5ec5ff6f53
Maxim <maximsushkin02@gmail.com>
2024-09-15 11:51:03 +0300

    moved unused schemas

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,16 +1,12 @@
-import blockContent from './blockContent'
 import {category} from './category'
 import {currency} from './currency'
 import {deliveryCountry} from './delivery-country'
 import {deliveryMethod} from './delivery-methods'
 import {managers} from './managers'
-import post from './post'
 import {redemptionFee} from './redemption-fee'
 import {shop} from './shops'
 
 export const schemaTypes = [
-  post,
-  blockContent,
   category,
   currency,
   deliveryCountry,
